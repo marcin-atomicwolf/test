46ab3db9c91f68048f577d41bdf96596033c00a4
Marcin Priachin <marcin@atomicwolf.games>
2021-03-09 16:11:24 +0100

    change 5

--- test/Assets/Scripts/Editor/EditorBuild.cs
+++ test/Assets/Scripts/Editor/EditorBuild.cs
@@ -12,7 +12,7 @@ class EditorBuild
         string path = "Assets/SteamData.vdf";
 
         StreamReader reader = new StreamReader(path);
-        File.WriteAllLines(contentBuilderPath + "scripts/SteamData.vdf", new[] { reader.ReadToEnd().Replace("@@@@", "Version " + Application.version + " Date " + System.DateTime.Now.ToString("yyyy/MM/dd hh:mm")) });
+        File.WriteAllLines(contentBuilderPath + "scripts/SteamData.vdf", new[] { reader.ReadToEnd().Replace("@@@@", "Version " + Application.version + " Date " + System.DateTime.Now.ToString("yyyy/MM/dd HH:mm")) });
         reader.Close();
 
         string locationPathName = contentBuilderPath + "content/windows_content/";
